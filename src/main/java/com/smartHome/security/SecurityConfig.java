352aff1dc4efe16fc625bd3988f224021fd318dd
minhKhoa <trantuanminhkhoa2004@gmail.com>
2025-04-22 14:27:00 +0700

    fix security

--- src/main/java/com/smartHome/security/SecurityConfig.java
+++ src/main/java/com/smartHome/security/SecurityConfig.java
@@ -57,7 +57,7 @@ public class SecurityConfig {
             .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers("/auth/**").permitAll()
-                .requestMatchers("/ws").permitAll()
+                .requestMatchers("/ws/**").permitAll()
                 .requestMatchers(
                     "/v3/api-docs/**",
                     "/swagger-ui.html",
